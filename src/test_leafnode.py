18982055a4dcf1fdd9eece3bd24aedf93df7c8d6
jukkakansanaho <jukka.kansanaho@gmail.com>
2025-01-03 12:31:31 +0200

    feat: Add LeafNode and related unittests.

---
+++ src/test_leafnode.py
@@ -0,0 +1,51 @@
+import unittest
+
+from leafnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leafnode_to_html(self):
+        node1 = LeafNode("p", "Content for p leafnode")
+        self.assertEqual(node1.to_html(), f"<p>Content for p leafnode</p>")
+
+        props2 = {"href": "https://test.com", "target": "_blank"}
+        node2 = LeafNode("a", "Content for a leafnode", None, props2)
+        self.assertEqual(
+            node2.to_html(),
+            f'<a href="https://test.com" target="_blank">Content for a leafnode</a>',
+        )
+
+        props3 = {"href": "https://test.com", "target": "_blank"}
+        node3 = LeafNode(tag="a", value="Content for a leafnode", props=props3)
+        self.assertEqual(
+            node3.to_html(),
+            f'<a href="https://test.com" target="_blank">Content for a leafnode</a>',
+        )
+
+    def test_leafnode_to_html_no_tag(self):
+        node1 = LeafNode(None, "Content for empty tag leafnode")
+        self.assertEqual(node1.to_html(), f"Content for empty tag leafnode")
+
+    def test_leafnode_to_html_no_value(self):
+        node1 = LeafNode("p", None)
+        try:
+            self.assertEqual(node1.to_html(), f"<p></p>")
+        except ValueError as e:
+            self.assertEqual(type(e), ValueError)
+        else:
+            self.fail("ValueError not raised")
+
+    def test_leafnode_no_children(self):
+        children = ["b"]
+        props = {"foo": "bar"}
+        try:
+            node1 = LeafNode(
+                tag="p",
+                value="Content for leafnode with children",
+                children=children,
+                props=props,
+            )
+        except TypeError as e:
+            self.assertEqual(type(e), TypeError)
+        else:
+            self.fail("Type error not raised")
